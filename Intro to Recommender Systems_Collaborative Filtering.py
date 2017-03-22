20a2724a5c0189f80fb60f473899164e4031dbbd
Hailin <fuhailin@hbut.edu.cn>
2017-03-23 00:41:44 +0800

    no message

---
+++ Intro to Recommender Systems_Collaborative Filtering.py
@@ -0,0 +1,170 @@
+from DataHelper import *
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import mean_squared_error
+from EvaluationHelper import *
+
+def get_mse(pred, actual):
+    # Ignore nonzero terms.
+    pred = pred[actual.nonzero()].flatten()
+    actual = actual[actual.nonzero()].flatten()
+    return mean_squared_error(pred, actual)
+def slow_similarity(ratings, kind='user'):
+    if kind == 'user':
+        axmax = 0
+        axmin = 1
+    elif kind == 'item':
+        axmax = 1
+        axmin = 0
+    sim = np.zeros((ratings.shape[axmax], ratings.shape[axmax]))
+    for u in range(ratings.shape[axmax]):
+        for uprime in range(ratings.shape[axmax]):
+            rui_sqrd = 0.
+            ruprimei_sqrd = 0.
+            for i in range(ratings.shape[axmin]):
+                sim[u, uprime] = ratings[u, i] * ratings[uprime, i]
+                rui_sqrd += ratings[u, i] ** 2
+                ruprimei_sqrd += ratings[uprime, i] ** 2
+            sim[u, uprime] /= rui_sqrd * ruprimei_sqrd
+    return sim
+
+def fast_similarity(ratings, kind='user', epsilon=1e-9):
+    # epsilon -> small number for handling dived-by-zero errors
+    if kind == 'user':
+        sim = ratings.dot(ratings.T) + epsilon
+    elif kind == 'item':
+        sim = ratings.T.dot(ratings) + epsilon
+    norms = np.array([np.sqrt(np.diagonal(sim))])
+    return (sim / norms / norms.T)
+
+def predict_slow_simple(ratings, similarity, kind='user'):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        for i in range(ratings.shape[0]):
+            for j in range(ratings.shape[1]):
+                pred[i, j] = similarity[i, :].dot(ratings[:, j])\
+                             /np.sum(np.abs(similarity[i, :]))
+        return pred
+    elif kind == 'item':
+        for i in range(ratings.shape[0]):
+            for j in range(ratings.shape[1]):
+                pred[i, j] = similarity[j, :].dot(ratings[i, :].T)\
+                             /np.sum(np.abs(similarity[j, :]))
+
+        return pred
+
+def predict_fast_simple(ratings, similarity, kind='user'):
+    if kind == 'user':
+        return similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif kind == 'item':
+        return ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+
+
+def predict_topk(ratings, similarity, kind='user', k=40):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        for i in range(ratings.shape[0]):
+            top_k_users = [np.argsort(similarity[:, i])[:-k - 1:-1]]
+            for j in range(ratings.shape[1]):
+                pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users])
+                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+    if kind == 'item':
+        for j in range(ratings.shape[1]):
+            top_k_items = [np.argsort(similarity[:, j])[:-k - 1:-1]]
+            for i in range(ratings.shape[0]):
+                pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T)
+                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))
+
+    return pred
+
+
+def predict_nobias(ratings, similarity, kind='user'):
+    if kind == 'user':
+        user_bias = ratings.mean(axis=1)
+        ratings = (ratings - user_bias[:, np.newaxis]).copy()
+        pred = similarity.dot(ratings) / np.array([np.abs(similarity).sum(axis=1)]).T
+        pred += user_bias[:, np.newaxis]
+    elif kind == 'item':
+        item_bias = ratings.mean(axis=0)
+        ratings = (ratings - item_bias[np.newaxis, :]).copy()
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+        pred += item_bias[np.newaxis, :]
+
+    return pred
+
+
+def predict_topk_nobias(ratings, similarity, kind='user', k=40):
+    pred = np.zeros(ratings.shape)
+    if kind == 'user':
+        user_bias = ratings.mean(axis=1)
+        ratings = (ratings - user_bias[:, np.newaxis]).copy()
+        for i in range(ratings.shape[0]):
+            top_k_users = [np.argsort(similarity[:, i])[:-k - 1:-1]]
+            for j in range(ratings.shape[1]):
+                pred[i, j] = similarity[i, :][top_k_users].dot(ratings[:, j][top_k_users])
+                pred[i, j] /= np.sum(np.abs(similarity[i, :][top_k_users]))
+        pred += user_bias[:, np.newaxis]
+    if kind == 'item':
+        item_bias = ratings.mean(axis=0)
+        ratings = (ratings - item_bias[np.newaxis, :]).copy()
+        for j in range(ratings.shape[1]):
+            top_k_items = [np.argsort(similarity[:, j])[:-k - 1:-1]]
+            for i in range(ratings.shape[0]):
+                pred[i, j] = similarity[j, :][top_k_items].dot(ratings[i, :][top_k_items].T)
+                pred[i, j] /= np.sum(np.abs(similarity[j, :][top_k_items]))
+        pred += item_bias[np.newaxis, :]
+
+    return pred
+
+if __name__ == '__main__':
+    MyData = LoadMovieLens100k('Datas/ml-100k/u.data')
+    print(MyData.head())
+    n_users = MyData.user_id.unique().shape[0]
+    n_items = MyData.item_id.unique().shape[0]
+    print('Number of users = ' + str(n_users) + ' | Number of movies = ' + str(n_items))
+    train_data, test_data = SpiltData(MyData, SpiltRate=0.25)
+    train_data = pd.DataFrame(train_data)
+    test_data = pd.DataFrame(test_data)
+
+    # Create two user-item matrices, one for training and another for testing
+    train_data_matrix = np.zeros((n_users, n_items))
+    for line in train_data.itertuples():
+        train_data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+    test_data_matrix = np.zeros((n_users, n_items))
+    for line in test_data.itertuples():
+        test_data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+    ratings = np.zeros((n_users, n_items))
+    for row in MyData.itertuples():
+        ratings[row[1] - 1, row[2] - 1] = row[3]
+
+    sparsity = float(len(ratings.nonzero()[0]))
+    sparsity /= (ratings.shape[0] * ratings.shape[1])
+    sparsity *= 100
+    print( 'Sparsity: {:4.2f}%'.format(sparsity))
+    train, test = train_test_split(ratings)
+
+    user_similarity = fast_similarity(train, kind='user')
+    item_similarity = fast_similarity(train, kind='item')
+    print(item_similarity[:4, :4])
+
+    item_prediction = predict_fast_simple(train, item_similarity, kind='item')
+    user_prediction = predict_fast_simple(train, user_similarity, kind='user')
+
+    print('User-based CF MSE: ' + str(get_mse(user_prediction, test)))
+    print('Item-based CF RMSE: ' + str(RMSE(item_prediction, test)))
+    pred = predict_topk(train, user_similarity, kind='user', k=40)
+    print('Top-k User-based CF RMSE: ' + str(RMSE(pred, test)))
+
+    pred = predict_topk(train, item_similarity, kind='item', k=40)
+    print('Top-k Item-based CF RMSE: ' + str(RMSE(pred, test)))
+
+    user_pred = predict_topk_nobias(train, user_similarity, kind='user')
+    print('Bias-subtracted User-based CF RMSE: ' + str(RMSE(user_pred, test)))
+
+    user_pred = predict_nobias(train, user_similarity, kind='user')
+    print('Bias-subtracted User-based CF RMSE: ' + str(RMSE(user_pred, test)))
+    item_pred = predict_nobias(train, item_similarity, kind='item')
+    print('Bias-subtracted Item-based CF RMSE: ' + str(RMSE(item_pred, test)))
